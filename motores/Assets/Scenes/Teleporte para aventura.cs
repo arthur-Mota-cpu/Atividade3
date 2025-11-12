bc3792824f26a019f0d754bb19118289ea488dab
arthur-Mota-cpu <arthur.mota@escolar.ifrn.edu.br>
2025-11-12 16:06:08 -0300

    salvamento no nucleo de jogos 12/11/2025

--- motores/Assets/Scenes/Teleporte para aventura.cs
+++ motores/Assets/Scenes/Teleporte para aventura.cs
@@ -3,11 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Porta : MonoBehaviour
 {
+    public string nomeDaCena;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("fase 4");
+            SceneManager.LoadScene(nomeDaCena);
         }
     }
 }
